639e47f1dc7f265d9b4472d44add1b0f04db86fb
Hyundoo, Jin <gusen0927@gmail.com>
2018-02-13 21:32:30 +0900

    카카오 테스트

--- Code/chatbot/chatbot/bschat/views.py
+++ Code/chatbot/chatbot/bschat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def keyboard(request) :
+    return JsonResponse({
+        'type' : 'buttons',
+        'buttons' : ['1', '2']
+    })
