7b5f9e12e74150d6dc49a4435b090c13731bfd09
Hyundoo, Jin <gusen0927@gmail.com>
2018-02-13 17:55:55 +0900

    aws

---
+++ Code/chatbot/chatbot/bschat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BschatConfig(AppConfig):
+    name = 'bschat'
---
+++ Code/chatbot/chatbot/chatbot/wsgi.py
@@ -0,0 +1,10 @@
+import os, sys
+
+from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/jin-lab/anaconda3/envs/chatbot/lib/python3.6/site-packages/django')
+sys.path.append('/home/jin-lab/')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbot.settings")
+
+application = get_wsgi_application()
