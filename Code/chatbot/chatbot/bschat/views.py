8b82268d61d6fac8669b138b848dba0a5a23684a
Ubuntu <ubuntu@ip-172-31-1-53.ap-northeast-2.compute.internal>
2018-02-17 14:38:31 +0000

    왜안돼

--- Code/chatbot/chatbot/bschat/views.py
+++ Code/chatbot/chatbot/bschat/views.py
@@ -5,23 +5,21 @@ from django.db import connection
 
 def keyboard(request):
     return JsonResponse({
-        'message': {
-            'text' : '안녕하세요? 시즌별 메이저리그 팀 성적을 알려드리는 챗봇입니다.'
-        }
+        'type' : 'buttons',
+        'buttons' : ['1', '2']
     })
-
 
 @csrf_exempt
 def message(request):
     message = ((request.body).decode('utf-8'))
     return_json_str = json.loads(message)
     return_str = return_json_str['content']
-
-    requestmode = return_str.encode('utf-8')
-    if '시즌' in requestmode :
-        season = int(requestmode[requestmode.find('시즌') - 1 : requestmode.find('시즌')])
     return JsonResponse({
-        'message' : {
-            'text' : season
-        }
-    })
+        'message': {
+            'text': "you type "+return_str+"!"
+            },
+        'keyboard': {
+            'type': 'buttons',
+            'buttons': ['1','2']
+            }
+        })
