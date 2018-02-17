93b5cfeb2aa618aba84b26a96cd7ab1a1841499e
jeon <wjsghdwns730>
2018-02-17 16:52:29 +0900

    행니마 이것좀 푸쉬 좀 해주이소

--- Code/chatbot/chatbot/chatbot/settings.py
+++ Code/chatbot/chatbot/chatbot/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lahman2016', # DB명
+        'USER': 'root', # 데이터베이스 계정
+        'PASSWORD': 'Asdf1020*', # 계정 비밀번호
+        'HOST': '127.0.0.1', # 데이테베이스 주소(IP)
+        'PORT': '3306', # 데이터베이스 포트(보통은 3306)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
